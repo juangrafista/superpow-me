2b7d00305c8a5ec1d310e510b5e4bebd8b36a0b1
juangrafista <juangrafista@gmail.com>
2020-11-18 22:33:59 -0500

    trying to fix error on deploy

--- components/post-body.js
+++ components/post-body.js
@@ -2,7 +2,32 @@ import markdownStyles from './markdown-styles.module.css'
 import BlockContent from '@sanity/block-content-to-react'
 import client from '../lib/sanity'
 
-import BlockRenderer from '../styles/blockRenderer'
+const BlockRenderer = (props) => {
+  const { style } = props.node
+
+  if (style === 'h2') {
+    return (
+      <h2 className='text-2xl text-red-500 font-bold leading-loose pb-3'>
+        {props.children}
+      </h2>
+    )
+  }
+
+  if (style === 'normal') {
+    return <p className='text-blue-500 font-bold pb-2'>{props.children}</p>
+  }
+
+  if (style === 'blockquote') {
+    return (
+      <blockquote className='p-4 italic border-l-4 bg-neutral-100 text-neutral-600 border-neutral-500 quote pb-3'>
+        {props.children}
+      </blockquote>
+    )
+  }
+
+  // Fall back to default handling
+  return BlockContent.defaultSerializers.types.block(props)
+}
 
 export default function PostBody({ content }) {
   return (
